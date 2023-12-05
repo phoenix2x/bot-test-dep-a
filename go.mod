c288d5ceccc2cd75163dd60357b020e4183bb8fa
iharzhavarankau <izhavarankau@fubo.tv>
2023-12-05 17:45:09 -0500

    v1

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/phoenix2x/bot-test-dep-a
+
+go 1.21.4
+
+require github.com/phoenix2x/bot-test-dep-b v1.0.0
---
+++ something.go
@@ -0,0 +1,11 @@
+package bot_test_dep_a
+
+import (
+	"fmt"
+
+	dummy "github.com/phoenix2x/bot-test-dep-b"
+)
+
+func F() {
+	fmt.Println(dummy.Dummy)
+}
